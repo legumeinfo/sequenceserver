c57b9fb51b4e1b37d41b96565486042e6a3ff781
Sven Gato Redsun <svengato@svengato.com>
2023-04-10 12:44:48 -0600

    Add link that exports BLAST results (HSPs) to a JBrowse instance

--- lib/sequenceserver/links.rb
+++ lib/sequenceserver/links.rb
@@ -79,6 +79,62 @@ module SequenceServer
           }
       end
     end
+
+    def jbrowse
+      # Create the JBrowse link only for genomic regions, not genes
+      if id.match("gnm.*\.ann")
+        return
+      end
+
+      # Prepare other fields besides sessionTracks
+      locStart = coordinates[1][0].to_s()
+      locEnd = coordinates[1][1].to_s()
+      assembly = id.match('^.+gnm\d+')[0]
+      tracks = 'sequenceserver_track'
+
+      # Prepare the sessionTracks field
+      sessionTracks = '[{"type":"FeatureTrack","trackId":"sequenceserver_track","name":"SequenceServer Hits","assemblyNames":["' + assembly + '"],"adapter":{"type":"FromConfigAdapter","features":['
+      hsps.each_with_index do |hsp, i|
+        if i > 0
+          sessionTracks << ','
+        end
+        uid = query.id + '-' + hsp.number.to_s()
+        sessionTracks << '{'
+          sessionTracks << '"uniqueId":"' + uid + '"'
+          sessionTracks << ',"refName":"' + id + '"'
+          sessionTracks << ',"start":' + hsp.sstart.to_s()
+          sessionTracks << ',"end":' + hsp.send.to_s()
+          sessionTracks << ',"name":"' + uid + '"'
+          sessionTracks << ',"assembly":"' + assembly + '"'
+          sessionTracks << ',"bit_score":' + hsp.bit_score.round(2).to_s()
+          sessionTracks << ',"score":' + hsp.score.to_s()
+          sessionTracks << ',"evalue":' + ("%#.2e" % hsp.evalue).to_s()
+          sessionTracks << ',"identity":"' + hsp.identity.to_s() + '/' + hsp.length.to_s()
+          sessionTracks << ' (' + (100.0*hsp.identity/hsp.length).round(1).to_s() + '%)"'
+          sessionTracks << ',"positives":"' + hsp.positives.to_s() + '/' + hsp.length.to_s()
+          sessionTracks << ' (' + (100.0*hsp.positives/hsp.length).round(1).to_s() + '%)"'
+          sessionTracks << ',"gaps":"' + hsp.gaps.to_s() + '/' + hsp.length.to_s()
+          sessionTracks << ' (' + (100.0*hsp.gaps/hsp.length).round(1).to_s() + '%)"'
+          sessionTracks << ',"hit_frame":' + hsp.sframe.to_s()
+        sessionTracks << '}'
+      end
+      sessionTracks << ']}}]'
+
+      # Assemble the JBrowse link URL from the fields above
+      url = 'https://dev.peanutbase.org/tools/jbrowse2/'
+      url << '?loc=' + id + ':' + locStart + '-' + locEnd
+      url << '&assembly=' + assembly
+      url << '&tracks=' + tracks
+      url << '&sessionTracks=' + sessionTracks
+
+      # Return the link object
+      {
+        order: 3,
+        title: 'JBrowse2',
+        url: url,
+        icon: 'fa-external-link'
+      }
+    end
   end
 end
 
