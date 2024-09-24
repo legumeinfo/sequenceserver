e310fa39b93b477574ce1142c69bab895b459ae5
adf <adf@ncgr.org>
2024-09-24 17:58:52 -0400

    num_jobs => 5, jbrowse2 links, remove old linkouts

--- lib/sequenceserver/links.rb
+++ lib/sequenceserver/links.rb
@@ -60,29 +60,99 @@ module SequenceServer
     #     query_coords = coordinates[0]
     #     hit_coords = coordinates[1]
 
-    def lis
-      if id.match("gnm.*\.ann") 
-          url = "https://www.legumefederation.org/en/linkout_mgr/?gene=" + id
-          {
-            order: 2,
-            title: 'LIS gene linkouts',
-            url:   url,
-            icon:  'fa-link'
-          }
-      else
-          url = ""
-          if coordinates[1][0] <= coordinates[1][1]
-            url = "https://www.legumefederation.org/en/linkout_mgr/?seqname=" + id + "&start=" + coordinates[1][0].to_s() + "&end=" + coordinates[1][1].to_s()
-          else
-            url = "https://www.legumefederation.org/en/linkout_mgr/?seqname=" + id + "&start=" + coordinates[1][1].to_s() + "&end=" + coordinates[1][0].to_s()
-          end
-          {
-            order: 2,
-            title: 'LIS region linkouts',
-            url:   url,
-            icon:  'fa-link'
-          }
+#    def lis
+#      if id.match("gnm.*\.ann") 
+#          url = "https://www.legumefederation.org/en/linkout_mgr/?gene=" + id
+#          {
+#            order: 2,
+#            title: 'LIS gene linkouts',
+#            url:   url,
+#            icon:  'fa-link'
+#          }
+#      else
+#          url = "https://www.legumefederation.org/en/linkout_mgr/?seqname=" + id + "&start=" + coordinates[1][0].to_s() + "&end=" + coordinates[1][1].to_s()
+#          {
+#            order: 2,
+#            title: 'LIS region linkouts',
+#            url:   url,
+#            icon:  'fa-link'
+#          }
+#      end
+#    end
+
+    def jbrowse
+      # Create the JBrowse link only for genomic regions, not genes
+      if id.match("gnm.*\.ann")
+        return
       end
+
+      # Prepare other fields besides sessionTracks
+      #locStart = coordinates[1][0].to_s()
+      #locEnd = coordinates[1][1].to_s()
+      locStart = hsps[0].sstart.to_s()
+      locEnd = hsps[0].send.to_s()
+      assembly = id.match('^.+gnm\d+')[0]
+      tracks = 'sequenceserver_track'
+
+      # Prepare the sessionTracks field
+      sessionTracks = '[{"type":"FeatureTrack","trackId":"sequenceserver_track","name":"SequenceServer Hits","assemblyNames":["' + assembly + '"],"adapter":{"type":"FromConfigAdapter","features":['
+      hsps.each_with_index do |hsp, i|
+        if i > 0
+          sessionTracks << ','
+        end
+        uid = query.id + '-' + hsp.number.to_s()
+        # Ensure sstart < send
+        sstart = hsp.sstart
+        send = hsp.send
+        if sstart > send
+          tmp = send
+          send = sstart
+          sstart = tmp
+        end
+        # Put evalue in scientific notation only if nonzero (positive)
+        e = '0'
+        if hsp.evalue > 0
+          e = ("%#.2e" % hsp.evalue).to_s()
+        end
+        sessionTracks << '{'
+          sessionTracks << '"uniqueId":"' + uid + '"'
+          sessionTracks << ',"refName":"' + id + '"'
+          sessionTracks << ',"start":' + sstart.to_s()
+          sessionTracks << ',"end":' + send.to_s()
+          sessionTracks << ',"name":"' + uid + '"'
+          sessionTracks << ',"assembly":"' + assembly + '"'
+          sessionTracks << ',"bit_score":' + hsp.bit_score.round(2).to_s()
+          sessionTracks << ',"score":' + hsp.score.to_s()
+          sessionTracks << ',"evalue":' + e
+          sessionTracks << ',"identity":"' + hsp.identity.to_s() + '/' + hsp.length.to_s()
+          sessionTracks << ' (' + (100.0*hsp.identity/hsp.length).round(1).to_s() + '%)"'
+          sessionTracks << ',"positives":"' + hsp.positives.to_s() + '/' + hsp.length.to_s()
+          sessionTracks << ' (' + (100.0*hsp.positives/hsp.length).round(1).to_s() + '%)"'
+          sessionTracks << ',"gaps":"' + hsp.gaps.to_s() + '/' + hsp.length.to_s()
+          sessionTracks << ' (' + (100.0*hsp.gaps/hsp.length).round(1).to_s() + '%)"'
+          sessionTracks << ',"hit_frame":' + hsp.sframe.to_s()
+        sessionTracks << '}'
+      end
+      sessionTracks << ']}}]'
+
+      # Assemble the JBrowse link URL from the fields above
+      url = 'https://medicago.legumeinfo.org/tools/jbrowse2/'
+      if locStart <= locEnd
+        url << '?loc=' + id + ':' + locStart + '-' + locEnd
+      else
+        url << '?loc=' + id + ':' + locEnd + '-' + locStart
+      end
+      url << '&assembly=' + assembly
+      url << '&tracks=' + tracks
+      url << '&sessionTracks=' + sessionTracks
+
+      # Return the link object
+      {
+        order: 3,
+        title: 'JBrowse2',
+        url: url,
+        icon: 'fa-external-link'
+      }
     end
   end
 end
